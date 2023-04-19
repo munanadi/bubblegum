ecf6a4249c6a1316c4bdcec7d2d4dedc0bd59a4d
nana <ajibaa@gmail.com>
2023-04-19 22:56:12 +0530

    styles: add spinner, photograph icon

--- components/Spinner/index.tsx
+++ components/Spinner/index.tsx
@@ -1,10 +1,15 @@
-const Spinner = () => {
+import { ReactNode, FC } from "react";
+
+interface Props {
+    [key: string]: any;
+}
+const Spinner: FC<Props> = props => {
+    let classname =
+        "animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-gray-600 rounded-full";
+    classname += " " + props.className;
+
     return (
-        <div
-            className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-gray-600 rounded-full"
-            role="status"
-            aria-label="loading"
-        >
+        <div className={classname} role="status" aria-label="loading">
             <span className="sr-only">Loading...</span>
         </div>
     );
