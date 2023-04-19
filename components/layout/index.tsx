e0a2cbb630bcbf9748d6806947b3e52c619cf36b
nana <ajibaa@gmail.com>
2023-04-19 19:24:10 +0530

    chore: init gum, drive

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -2,12 +2,22 @@ import React, { FC, ReactNode } from "react";
 import NavBar from "../NavBar";
 import { ErrorBoundary } from "react-error-boundary";
 import Footer from "../Footer";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useGumStuff } from "@/hooks/useGumStuff";
+import { useShadowDrive } from "@/hooks/useShadowDrive";
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    // TODO: Should be initialized here
+    const {} = useGumStuff();
+    const {} = useShadowDrive(wallet, connection);
+
     return (
         <ErrorBoundary fallback={<div>Something went wrong</div>}>
             <NavBar />
