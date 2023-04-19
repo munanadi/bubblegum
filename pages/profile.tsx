488c8980b492b8747e59aef59974812a11cdac9a
nana <ajibaa@gmail.com>
2023-04-19 19:24:42 +0530

    [WIP] feat: write bubbles

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,25 +1,51 @@
-import { useGumStuff } from "@/hooks/useGumStuff";
-import { useShadowDrive } from "@/hooks/useShadowDrive";
 import { useAppState } from "@/store/AppState";
-import { StorageAccountResponse } from "@shadow-drive/sdk";
-import { FC, ReactNode, useEffect, useState } from "react";
+import { FC, ReactNode, useRef, useState } from "react";
 
 const Profile: FC<{ children: ReactNode }> = ({ children }) => {
     const wallet = useAppState(state => state.wallet);
-    const connected = useAppState(state => state.wallet?.connected);
-    const connection = useAppState(state => state.connection);
-    const sdk = useAppState(state => state.gumSdk);
 
     const userPDA = useAppState(state => state.userPDA);
     const profilePDA = useAppState(state => state.profilePDA);
 
-    const storageAccount = useAppState(state => state.storageAccount);
-
     const publickey = wallet?.publicKey;
 
-    const { drive } = useShadowDrive(wallet, connection);
+    const showInput = publickey && profilePDA && userPDA;
 
-    const { getOrCreateUser, getOrCreateProfile } = useGumStuff();
+    const handleBubble = (e: any) => {
+        e.preventDefault();
+        console.log(text);
+        console.log(imagesName);
+        console.log(images);
+    };
+
+    const [text, setText] = useState("");
+    const [images, setImages] = useState<any[]>([]);
+    const [imagesName, setImagesNames] = useState<any[]>([]);
+    const inputRef = useRef<any>(null);
+
+    function handleDrop(event: any) {
+        event.preventDefault();
+        const files = event.dataTransfer.files;
+
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            console.log(file);
+            setImagesNames([...imagesName, file.name]);
+            if (file.type.includes("image")) {
+                const reader = new FileReader();
+                reader.onload = function () {
+                    setImages([...images, reader.result?.toString()]);
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+
+        console.log("Uploaded", images);
+    }
+
+    function handleTextChange(event: any) {
+        setText(event.target.value);
+    }
 
     return (
         <>
@@ -29,11 +55,45 @@ const Profile: FC<{ children: ReactNode }> = ({ children }) => {
                     {publickey &&
                         (!profilePDA || !userPDA) &&
                         `Head to create profile to create profile`}
-                    {publickey &&
-                        profilePDA &&
-                        userPDA &&
-                        `Here is a list of your posts`}
                 </h1>
+                {showInput && (
+                    <>
+                        <div
+                            className="border-dashed border-2 border-gray-300 p-6"
+                            onDrop={handleDrop}
+                            onClick={() => {
+                                if (inputRef.current) {
+                                    inputRef.current.focus();
+                                }
+                            }}
+                        >
+                            <textarea
+                                className="w-full p-2 mb-4 border-0"
+                                value={text}
+                                ref={inputRef}
+                                onChange={handleTextChange}
+                            />
+                            <div className="flex flex-wrap">
+                                {images.map((image, index) => (
+                                    <img
+                                        src={image}
+                                        alt={`Uploaded file ${index}`}
+                                        key={index}
+                                        className="max-w-full h-auto mb-4 mr-4"
+                                    />
+                                ))}
+                            </div>
+                        </div>
+
+                        <button
+                            className="m-2 p-0.5 border-black border-2 rounded-md"
+                            type="submit"
+                            onClick={handleBubble}
+                        >
+                            Bubble
+                        </button>
+                    </>
+                )}
             </div>
         </>
     );
